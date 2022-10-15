08862173c346deea185570fa54281214c4ddaf4f
Pawikoski <pawikoski@gmail.com>
2022-10-15 11:22:50 +0200

    finished bout me section

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -5,9 +5,20 @@ from .models import About
 # Create your views here.
 def index(request):
     about_info = About.objects.first()
+    about = {
+        "technologies": ', '.join([technology.name for technology in about_info.technologies.all()]),
+        "tools": ', '.join([tool.name for tool in about_info.tools.all()]),
+        "operating_systems": ', '.join([operating_system.name for operating_system in about_info.operating_systems.all()]),
+        "ides": ', '.join([ide.name for ide in about_info.ides.all()]),
+        "frameworks": ', '.join([framework.name for framework in about_info.frameworks.all()]),
+        "total_exp_years": about_info.total_exp_years,
+        "current_employer": about_info.current_employer,
+        "current_employer_website": about_info.current_employer_website
+    }
+    
     
     context = {
-        "about": about_info
+        "about": about,
     }
 
     return render(request, "index.html", context=context)
