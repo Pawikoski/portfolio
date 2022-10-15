c48b2c516f69f20830dcdfda89423cee5f0152c9
Pawikoski <pawikoski@gmail.com>
2022-10-15 16:34:53 +0200

    expecting none from object

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -5,16 +5,18 @@ from .models import About, LatestProject, Social
 # Create your views here.
 def index(request):
     about_info = About.objects.first()
-    about = {
-        "technologies": ', '.join([technology.name for technology in about_info.technologies.all()]),
-        "tools": ', '.join([tool.name for tool in about_info.tools.all()]),
-        "operating_systems": ', '.join([operating_system.name for operating_system in about_info.operating_systems.all()]),
-        "ides": ', '.join([ide.name for ide in about_info.ides.all()]),
-        "frameworks": ', '.join([framework.name for framework in about_info.frameworks.all()]),
-        "total_exp_years": about_info.total_exp_years,
-        "current_employer": about_info.current_employer,
-        "current_employer_website": about_info.current_employer_website
-    }
+    about = None
+    if about_info:
+        about = {
+            "technologies": ', '.join([technology.name for technology in about_info.technologies.all()]),
+            "tools": ', '.join([tool.name for tool in about_info.tools.all()]),
+            "operating_systems": ', '.join([operating_system.name for operating_system in about_info.operating_systems.all()]),
+            "ides": ', '.join([ide.name for ide in about_info.ides.all()]),
+            "frameworks": ', '.join([framework.name for framework in about_info.frameworks.all()]),
+            "total_exp_years": about_info.total_exp_years,
+            "current_employer": about_info.current_employer,
+            "current_employer_website": about_info.current_employer_website
+        }
     socials = Social.objects.all()
     latest_projects = LatestProject.objects.all()
 
