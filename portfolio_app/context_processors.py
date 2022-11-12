8103250bdf33f5e5995ed3b8f71559b7472272c6
Pawikoski <pawikoski@gmail.com>
2022-11-12 19:15:38 +0100

    Filtering education queryset

--- portfolio_app/context_processors.py
+++ portfolio_app/context_processors.py
@@ -19,7 +19,7 @@ def base_processor(request):
         "contact": Contact.objects.first(),
         "languages": [{"name": lang.name, "full_stars": range(lang.level), "blank_stars": range(6 - lang.level)} for lang in Language.objects.all()],
         "currently_studying": CurrentlyStudying.objects.all(),
-        "education": Education.objects.all(),
+        "education": Education.objects.filter(language=site_language),
         "personal_data": Personal.objects.filter(language=site_language).first(),
         "skills": Skill.objects.all(),
         "credits": Credit.objects.all(),
